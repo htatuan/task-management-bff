c158ebab35aa4a42e9a48b03b73fae3906d13997
TECHVN\tuanhtay <tuanhtay@mti-tech.vn>
2023-12-07 09:43:02 +0700

    add user type

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -2,27 +2,35 @@ import { Args, Query, Resolver, Mutation } from '@nestjs/graphql';
 import { TaskType } from './task.type';
 import { TaskService } from './task.service';
 import { Task } from './proto/task';
-import { ParseIntPipe } from '@nestjs/common';
+import { ParseIntPipe, UseGuards } from '@nestjs/common';
 import { GraphQLError } from 'graphql';
 import { CreateTaskInput } from './dto/create-task.input';
 import { UpdateTaskInput } from './dto/update-task.input';
+import { GqlAuthGuard } from 'src/auth/graphql.auth.guard';
 
+@UseGuards(GqlAuthGuard)
 @Resolver(() => TaskType)
 export class TaskResolver {
   constructor(private taskService: TaskService) {}
 
   @Query(() => TaskType)
-  async findOneTask(@Args('id', ParseIntPipe) id: number): Promise<Task | GraphQLError> {
+  async findOneTask(
+    @Args('id', ParseIntPipe) id: number,
+  ): Promise<Task | GraphQLError> {
     return this.taskService.findOneTask({ id });
   }
 
   @Query(() => [TaskType])
-  async findAllTasks(@Args('ownerId', ParseIntPipe) ownerId: number): Promise<Task[] | GraphQLError> {
+  async findAllTasks(
+    @Args('ownerId', ParseIntPipe) ownerId: number,
+  ): Promise<Task[] | GraphQLError> {
     return this.taskService.findAllTasks({ ownerId });
   }
 
   @Mutation(() => Boolean)
-  async deleteTask(@Args('id', ParseIntPipe) id: number): Promise<boolean | GraphQLError> {
+  async deleteTask(
+    @Args('id', ParseIntPipe) id: number,
+  ): Promise<boolean | GraphQLError> {
     return this.taskService.removeTask({ id });
   }
 
